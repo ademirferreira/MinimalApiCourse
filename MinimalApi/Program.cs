44aadc19e8c3142b678d2d830d84b665fd3e9058
Ademir Ferreira <whoisademir@gmail.com>
2023-01-20 14:17:13 -0300

    Add migrations

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,6 +1,12 @@
+using Application.Abstractions;
+using Data;
+using Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
-
-
+var connectionString = builder.Configuration.GetConnectionString("Default");
+builder.Services.AddDbContext<SocialDbContext>(opt => opt.UseSqlServer(connectionString));
+builder.Services.AddScoped<IPostRepository, PostRepository>();
 var app = builder.Build();
 
 
