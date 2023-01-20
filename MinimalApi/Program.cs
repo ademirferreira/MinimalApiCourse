2d206345439e4629bf0f90ed33d7005619ff59e9
Ademir Ferreira <whoisademir@gmail.com>
2023-01-20 15:24:39 -0300

    CQRS with MediatR

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,12 +1,16 @@
 using Application.Abstractions;
+using Application.Posts.Commands;
 using Data;
 using Data.Repositories;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("Default");
 builder.Services.AddDbContext<SocialDbContext>(opt => opt.UseSqlServer(connectionString));
 builder.Services.AddScoped<IPostRepository, PostRepository>();
+builder.Services.AddMediatR(typeof(CreatePost));
+
 var app = builder.Build();
 
 
