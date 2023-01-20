747bfddc390c34edaee9684caedba70939f96f38
Ademir Ferreira <whoisademir@gmail.com>
2023-01-20 13:55:00 -0300

    Add PostRepository

---
+++ Data/SocialDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data;
+
+public class SocialDbContext : DbContext
+{
+    public SocialDbContext(DbContextOptions opt) : base(opt)
+    {
+        
+    }
+
+    public DbSet<Post> Posts { get; set; }
+}
