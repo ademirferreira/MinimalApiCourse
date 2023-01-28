7e412b26a5ecf5f1f1e94ab9ebf51b8259952d57
Ademir Ferreira <whoisademir@gmail.com>
2023-01-27 22:10:54 -0300

    Global exception handling

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -6,10 +6,21 @@ builder.RegisterServices();
 
 var app = builder.Build();
 
+app.Use(async (context, next) =>
+{
+    try
+    {
+        await next();
+    }
+    catch (Exception)
+    {
+        context.Response.StatusCode = 500;
+        await context.Response.WriteAsync("An error ocurred");
+    }
+});
+
 app.UseHttpsRedirection();
 
 app.RegisterEndpointDefinitions();
 
 app.Run();
-
-
